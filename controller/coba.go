c965179163acee2b9ed7410dcd16d6224ee31983
erditona <erditonaushaadam@gmail.com>
2023-03-17 16:40:15 +0700

    4

--- controller/coba.go
+++ controller/coba.go
@@ -2,10 +2,17 @@ package controller
 
 import (
 	"github.com/aiteung/musik"
+	cek "github.com/aiteung/presensi"
+	"github.com/erditona/ws-dito/config"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Homepage(c *fiber.Ctx) error {
 	ipaddr := musik.GetIPaddress()
 	return c.JSON(ipaddr)
+}
+
+func GetPresensi(c *fiber.Ctx) error {
+	ps := cek.GetPresensiCurrentMonth(config.Ulbimongoconn)
+	return c.JSON(ps)
 }
